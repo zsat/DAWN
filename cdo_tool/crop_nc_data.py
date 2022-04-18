4550f72a93538b54c3856f061053b8ba66997e22
Zack Sating <zacksating@gmail.com>
2022-04-18 03:54:11 +0000

    now crops general .nc data of given params

--- cdo_tool/crop_nc_data.py
+++ cdo_tool/crop_nc_data.py
@@ -3,22 +3,34 @@ from os import listdir
 from re import match
 from time import strftime
 
-INPUT_PATH, OUTPUT_PATH = "", ""
-IN_DAWN_SERVER = False
-if IN_DAWN_SERVER:
-  INPUT_PATH = "/mnt/gfs01/DAWN-ML/et_data_nc"
-  OUTPUT_PATH = "/mnt/gfs01/DAWN-ML/et_data_nc_IL"
-else:
-  INPUT_PATH = "."
-  OUTPUT_PATH = "."
-
 STATE = "IL"
+DATA_NAME = "LAI" # "et"
+FILENAME_REGEX = "^\d{7}\.nc$" if DATA_NAME == "et" else "^LAI\d{7}\.nc$"
+
+INPUT_PATH, OUTPUT_PATH = ".", "."
+IN_DAWN_SERVER = True
+
+if IN_DAWN_SERVER:
+  if DATA_NAME == "et":
+    INPUT_PATH = "/mnt/gfs01/DAWN-ML/et_data_nc"
+    OUTPUT_PATH = f"/mnt/gfs01/DAWN-ML/et_data_nc_{STATE}"
+  elif DATA_NAME == "LAI":
+    INPUT_PATH=  "/mnt/gfs01/DAWN-ML/MODIS_LAI_4DAWN_GeoTiff"
+    OUTPUT_PATH=  f"/mnt/gfs01/DAWN-ML/LAI_data_nc_{STATE}"
 
 arg0 = "cdo"
 arg1 = "sellonlatbox,-91.51,-87.52,36.98,42.49"
 
 """
-% cdo sellonlatbox,-91.51,-87.52,36.98,42.49 infile outfile
+this program will take all of the entire mainland US ET data in .nc format and
+crop out just Illinois (or whatever state you choose)
+
+it runs the following CDO command as a process, and CDO takes care of cropping
+the data and putting the output wherever we specify.
+
+% cdo sellonlatbox,-91.51,-87.52,36.98,42.49 infile.nc outfile.nc
+
+you'll have to specify the coordinates, these 4 are specific to Illinois
 """
 
 
@@ -31,7 +43,7 @@ count = 0
 for filename in filenames:
 
   # ensure we're running cdo on the correct .nc file
-  if not match("^\d{7}\.nc$", filename):
+  if not match(FILENAME_REGEX, filename):
     continue
   
   # good file, so run cdo on it; ALSO if you want to display the output of every file processed (it's a lot) set capture_output=False
